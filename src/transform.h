53d95bdeec459622d22eb211aa3c377f988b3488
Veselin Georgiev <anrieff@gmail.com>
2013-11-24 12:32:17 +0200

    Fix whitespace in transform.h (spaces->tabs), and use a bit more clever code in Transform::rotate().

--- src/transform.h
+++ src/transform.h
@@ -5,71 +5,65 @@
 #include "matrix.h"
 #include "util.h"
 
+/// A transformation class, which implements model-view transform. Objects can be
+/// arbitrarily scaled, rotated and translated.
 class Transform {
-
 public:
+	Transform() { reset(); }
 
-    Transform() { reset(); }
+	void reset() {
+		transform = Matrix(1);
+		inverseTransform = inverseMatrix(transform);
+		offset.makeZero();
+	}
 
-    void reset() {
-        transform = Matrix(1);
-        inverseTransform = inverseMatrix(transform);
-        offset.makeZero();
-    }
+	void scale(double X, double Y, double Z) {
+		Matrix scaling(X);
+		scaling.m[1][1] = Y;
+		scaling.m[2][2] = Z;
 
-    void scale(double X, double Y, double Z) {
-        Matrix scaling(X);
-        scaling.m[1][1] = Y;
-        scaling.m[2][2] = Z;
+		transform = transform * scaling;
+		inverseTransform = inverseMatrix(scaling) * inverseTransform;
+	}
 
-        transform = transform * scaling;
-        inverseTransform = inverseMatrix(scaling) * inverseTransform;
-    }
+	void rotate(double yaw, double pitch, double roll) {
+		transform = transform *
+			rotationAroundX(toRadians(pitch)) *
+			rotationAroundY(toRadians(yaw)) *
+			rotationAroundZ(toRadians(roll));
+		inverseTransform = inverseMatrix(transform);
+	}
 
-    void rotate(double yaw, double pitch, double roll) {
-        transform = transform *
-                        rotationAroundX(toRadians(pitch)) *
-                        rotationAroundY(toRadians(yaw)) *
-                        rotationAroundZ(toRadians(roll));
-        inverseTransform =
-                        inverseMatrix(rotationAroundZ(toRadians(roll))) *
-                        inverseMatrix(rotationAroundY(toRadians(yaw))) *
-                        inverseMatrix(rotationAroundX(toRadians(pitch))) *
-                        inverseTransform;
-    }
+	void translate(const Vector& V) {
+		offset = V;
+	}
 
-    void translate(const Vector& V) {
-        offset = V;
-    }
+	Vector point(Vector P) {
+		P = P * transform;
+		P = P + offset;
 
-    Vector point(Vector P) {
-        P = P * transform;
-        P = P + offset;
+		return P;
+	}
 
-        return P;
-    }
+	Vector undoPoint(Vector P) {
+		P = P - offset;
+		P = P * inverseTransform;
 
-    Vector undoPoint(Vector P) {
-        P = P - offset;
-        P = P * inverseTransform;
+		return P;
+	}
 
-        return P;
-    }
+	Vector direction(const Vector& dir) {
+		return dir * transform;
+	}
 
-    Vector direction(Vector dir) {
-        return dir = dir * transform;
-    }
-
-    Vector undoDirection(Vector dir) {
-        return dir = dir * inverseTransform;
-    }
+	Vector undoDirection(const Vector& dir) {
+		return dir * inverseTransform;
+	}
 
 private:
-
-    Matrix transform;
-    Matrix inverseTransform;
-    Vector offset;
-
+	Matrix transform;
+	Matrix inverseTransform;
+	Vector offset;
 };
 
 #endif // __TRANSFORM_H__
