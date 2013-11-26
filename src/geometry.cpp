91d5fa2d3b20358764958eca9de106966e74af4a
Veselin Georgiev <anrieff@gmail.com>
2013-11-27 01:54:32 +0200

    Explain the code in Node::intersect().
    More specifically, the fix that implements the "bonus" from
    Homework 5/Medimum/Task1 is explained in more detail here.

--- src/geometry.cpp
+++ src/geometry.cpp
@@ -207,23 +207,43 @@ bool CsgDiff::intersect(Ray ray, IntersectionData& data)
 	 return true;
 }
 
-
+// intersect a ray with a node, considering the Model transform attached to the node.
 bool Node::intersect(Ray ray, IntersectionData& data)
 {
+	// convert the ray into the canonic space of the object:
 	ray.start = transform.undoPoint(ray.start);
 	ray.dir = transform.undoDirection(ray.dir);
-	double oldDist = data.dist;
+	
+	// save the old "best dist", in case we need to restore it later
+	double oldDist = data.dist; // *(1)
 	double rayDirLength = ray.dir.length();
-	data.dist *= rayDirLength;
-	ray.dir.normalize();
-	bool res = geom->intersect(ray, data);
-	if (!res) {
-		data.dist = oldDist;
+	data.dist *= rayDirLength;  // (2)
+	ray.dir.normalize();        // (3)
+	if (!geom->intersect(ray, data)) {
+		data.dist = oldDist;    // (4)
 		return false;
 	}
 	data.normal = normalize(transform.direction(data.normal));
 	data.p = transform.point(data.p);
-	data.dist /= rayDirLength;
+	data.dist /= rayDirLength;  // (5)
 	return true;
+	
+	/*
+	 * ^^^
+	 * Explanation for the numbered lines.
+	 *
+	 * Since the geometries in the scene may use different transforms, the only universal
+	 * coordinate system are the world coords. The applies to distances, too. We use data.dist
+	 * for culling geometries that won't improve the closest dist we've found so far in raytrace().
+	 * So, if a transform contains scaling, we need to adjust data.dist so that it's still valid
+	 * in the object's canonic space. Since ray.dir undergoes scaling as well, we use its length
+	 * to multiply the data.dist in (2). This essentially transforms the distance from world space
+	 * to object's canonic space. We save the old value of the distance in (1), in case there isn't
+	 * an intersection (4). Of course, Geometry::intersect assumes that ray.dir is an unit vector,
+	 * and we cater for that in (3). Finally, when the results arrive, convert the data.dist back
+	 * from object to world space (5).
+	 *
+	 * This implements the "bonus" from HW5/medium/group1.
+	 */
 }
 
